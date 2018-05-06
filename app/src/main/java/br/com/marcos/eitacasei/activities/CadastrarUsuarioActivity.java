50c37088b6b2177d124485d8737973bce6753be6
Marcos Aurélio Macedo da Rocha Júnior <marcosau_2809@hotmail.com>
2018-05-05 23:26:27 -0300

    Commit inicial do projeto

---
+++ app/src/main/java/br/com/marcos/eitacasei/activities/CadastrarUsuarioActivity.java
@@ -0,0 +1,10 @@
+package br.com.marcos.eitacasei.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by Marcos on 05/05/18.
+ */
+
+public class CadastrarUsuarioActivity extends Activity {
+}
